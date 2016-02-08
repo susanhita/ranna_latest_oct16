cb070b3613687aeafe6fdc259787a4d47f013090
susanhita <susanhita@gmail.com>
2016-02-08 18:07:54 +0530

    Added splash screen

---
+++ app/src/main/java/com/habijabi/mealplanner/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.habijabi.mealplanner;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by susanhita on 08-02-2016.
+ */
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
