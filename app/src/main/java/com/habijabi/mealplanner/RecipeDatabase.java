68aa0f7c79f2bd48d9ed3a33addd6f501e703019
susanhita <susanhita@gmail.com>
2016-02-20 16:52:43 +0530

    adjusted scrollbar in create recipe. Aligned text in activity_recipe. Aligned add ingredients button to center

--- app/src/main/java/com/habijabi/mealplanner/RecipeDatabase.java
+++ app/src/main/java/com/habijabi/mealplanner/RecipeDatabase.java
@@ -14,7 +14,7 @@ import java.net.URI;
 
 public class RecipeDatabase  extends SQLiteOpenHelper {
     private static final int DB_VERSION=1;
-    private static final String DB_NAME="RecipeDatabase0";
+    private static final String DB_NAME="RecipeDatabase";
 
     RecipeDatabase(Context context){
         super(context,DB_NAME,null,DB_VERSION);
@@ -28,9 +28,9 @@ public class RecipeDatabase  extends SQLiteOpenHelper {
 
 
         Uri path1=Uri.parse("android.resource://com.habijabi.mealplanner/"+R.drawable.pancake);
-        String recipe_description="\n" +
-                "    Combine milk with vinegar in a medium bowl and set aside for 5 minutes to \"sour\".\n" +
-                "    Combine flour, sugar, baking powder, baking soda, and salt in a large mixing bowl. Whisk egg and butter into \"soured\" milk. Pour the flour mixture into the wet ingredients and whisk until lumps are gone.\n" +
+        String recipe_description="\n" + "Ingredients needed\n"+
+                "2 eggs \n  1 cup flour \n 1/2 cup sugar \n 1 tbps butter \n 1/2 teaspoon baking powder \n 1/2 cup milk\n\n"+
+                "    Combine flour, sugar, baking powder, baking soda in a large mixing bowl. Whisk egg and butter into milk. Pour the flour mixture into the wet ingredients and whisk until lumps are gone." +
                 "    Heat a large skillet over medium heat, and coat with cooking spray. Pour 1/4 cupfuls of batter onto the skillet, and cook until bubbles appear on the surface. Flip with a spatula, and cook until browned on the other side.\n";
         insertDrink(db, "pancake",recipe_description,path1 );
 
