d1bb02d9d50fbdd2b3f7a1238aae08d8f3d47fea
susanhita <susanhita@gmail.com>
2016-03-04 09:21:40 +0530

    random recipe fixed(it didnt have row value)

--- app/src/main/java/com/habijabi/mealplanner/RandomReceipe.java
+++ app/src/main/java/com/habijabi/mealplanner/RandomReceipe.java
@@ -9,36 +9,61 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 public class RandomReceipe extends Activity {
+    int max;
 
 
+    public static final String EXTRA_RECIPENO = "drinkNo";
 
-            public static final String EXTRA_RECIPENO = "drinkNo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_receipe);
+        ActionBar actionBar = getActionBar();
+        new RandomDB().execute();
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_random_receipe);
-                ActionBar actionBar=getActionBar();
-                int id,max,min;
-                max=1;
-                min=1;
+    }
 
-                Random rand = new Random();
-                id = 1 + rand.nextInt((max - min) + 1);
+    private class RandomDB extends AsyncTask<String, String, String> {
 
-                Intent intent = new Intent(this, RecipeActivity.class);
-                intent.putExtra(RecipeActivity.EXTRA_RECIPENO, (int) id);
-                startActivity(intent);
-                finish();
+        protected void onPreExecute() {
+            System.gc();
+        }
 
+        protected String doInBackground(String...x) {
 
+            SQLiteOpenHelper recipedb = new RecipeDatabase(RandomReceipe.this);
+            SQLiteDatabase db = recipedb.getWritableDatabase();
+            try {
+                Cursor cursor = db.rawQuery("Select count(*) from RECIPE ", null);
+                cursor.moveToFirst();
+                String row_number = cursor.getString(0);
+                return row_number;
+            } catch (SQLiteException e) {
+                return "Database problem";
             }
+        }
+
+        protected void onPostExecute(String row_number) {
+            Log.v("XXXXXXXXXXXXXXXXX", row_number);
+            int max = Integer.parseInt(row_number);
+            int min = 1;
+            Random rand = new Random();
+            int id = 1 + rand.nextInt((max - min) + 1);
+            Log.v("rowwwwwwwwww", String.format("%s",id));
 
 
+            Intent intent = new Intent(RandomReceipe.this, RecipeActivity.class);
+            intent.putExtra(RecipeActivity.EXTRA_RECIPENO, (int) id);
+            startActivity(intent);
+            finish();
+
+        }
+    }
 
 }
